a4910cc8b1d7e1b41be28275d2d8bc1dd01ccc3a
keliu0530 <liu1754@purdue.edu>
2018-03-19 21:22:27 -0400

    update

--- group/kde.py
+++ group/kde.py
@@ -11,25 +11,32 @@ from os import listdir
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import time
+import sys
+from matplotlib.patches import Circle
+from math import sqrt 
 
 x_min = 112000
 x_max = 288000
 y_min = 210000
 y_max = 275000
-cell = 1000
 
-global x_grid, y_grid, W, T
+global x_grid, y_grid, W, t1, t2, cell, x_label, y_label
+cell = 1000
 x_grid = range(x_min + cell/2, x_max + cell/2, cell)
 y_grid = range(y_min + cell/2, y_max + cell/2, cell)
-ext = [x_min/1000, x_max/1000, y_min/1000, y_max/1000]
+ext = [x_min/cell, x_max/cell, y_min/cell, y_max/cell]
 W = 2
-T = 1
+x_label = ['120km', '140km', '160km', '180km', '200km', '220km', '240km', '260km', '280km']
+y_label = ['220km', '240km', '260km', '280km']
+
 
 
 def kdemap(t_start, t_end):
+    if("prob-" + str(t_start) + "-" + str(t_end) + "-" + str(cell) + ".npy" in os.listdir("./")):
+        prob = np.load("prob-" + str(t_start) + "-" + str(t_end) + "-" + str(cell) + ".npy")
+        return prob
     if(str(t_start) + "-" + str(t_end) + ".npy" in os.listdir("./")):
         values = np.load(str(t_start) + "-" + str(t_end) + ".npy")
-#        return values
     else:
         db = MySQLdb.connect(host="keliudb.corzaau5yusv.us-east-2.rds.amazonaws.com",    # your host, usually localhost
                              user="keliu",         # your username
@@ -64,44 +71,60 @@ def kdemap(t_start, t_end):
     plt.colorbar(im, cax=cax)
 
     #cbar = fig.colorbar(cax)
-    fig.savefig('figures/kde' + str(t_start) + '-' + str(t_end) + '.png')   # save the figure to file
+    fig.savefig('figures/kde' + str(t_start) + '-' + str(t_end) + '-' + str(cell) + '.png')   # save the figure to file
+    print "KDE map saved..."
+    np.save("prob-" + str(t_start) + "-" + str(t_end) + "-" + str(cell) + ".npy", prob)
+    return prob
 
 
-#kdemap(1503916200, 1503919800)
-kdemap(1503927000, 1503930600)
+#t1 = kdemap(1503916200, 1503919800) #8.28.2017 6:30am to 7:30am
+t1 = kdemap(1503948600, 1503952200) #8.28.2017 3:30am to 4:30pm
+print "Done with t1..."
+#t2 = kdemap(1503927000, 1503930600) #8.28.2017 9:30am to 10:30am
+t2 = kdemap(1503959400, 1503963000) #8.28.2017 6:30pm to 7:30pm
+print "Done with t2..."
 
-#def FMap(kde, x, y):
-#    xyt = np.zeros(3)
-#    t = len(kde)/2
-#    ft = kde[t][x][y]
-#    for p in range(-W, W+1):
-#        for q in range(-W, W+1):
-#            for r in range(-T, T+1):
-#                ftr = kde[t+r][x+p][y+q]
-#                if sqrt(p*p + q*q) != 0:
-#                    xyt = xyt + np.array([p, q, r])*ft*ftr/sqrt(p*p + q*q)
-#    return xyt*(10**22)
+def FMap(x, y):
+    t = t2 - t1
+    vector = np.zeros(2)
+    for p in range(-W, W+1):
+        for q in range(-W, W+1):
+            if(sqrt(p*p + q*q) != 0):
+#                vector = vector + np.array([p, q])*(t1[x][y]*t2[x+p][y+q]/(p*p + q*q))
+                vector = vector + np.array([p, q])*(t[x+p][y+q]/(p*p + q*q))
+    return vector
 
 
-
-#u = []
-#v = []
-#mv = []
-#if(len(x_grid) - W > W and len(y_grid) - W > W):
-#    for i in range(W, len(y) - W):
-#        x_tr = []
-#        y_tr = []
-#        h = []
-#        for j in range(W, len(x) - W):
-#            result = FMap(kde, j, i)
-#            x_tr.append(result[0])
-#            y_tr.append(result[1])
-#            h.append([result[0], result[1]])
-#        u.append(x_tr)
-#        v.append(y_tr)
-#        mv.append(h)
-#    U = np.asarray(u)
-#    V = np.asarray(v)
-#    [X, Y] = np.meshgrid(x[W: len(x)-W], y[W: len(y)-W])
-#plt.show()
+u = []
+v = []
+mv = []
+if(len(x_grid) - W > W and len(y_grid) - W > W):
+    for i in range(W, len(y_grid) - W):
+        x_tr = []
+        y_tr = []
+        h = []
+        for j in range(W, len(x_grid) - W):
+            result = FMap(j, i)
+            x_tr.append(result[0])
+            y_tr.append(result[1])
+            h.append([result[0], result[1]])
+        u.append(x_tr)
+        v.append(y_tr)
+        mv.append(h)
+    U = np.asarray(u)
+    V = np.asarray(v)
+    [X, Y] = np.meshgrid(x_grid[W: len(x_grid)-W], y_grid[W: len(y_grid)-W])
+    
+fig, ax = plt.subplots(figsize=(15,7))
+img = plt.imread("pr.JPG")
+ax.imshow(img, extent = ext)
+strm = ax.streamplot(X/cell, Y/cell, U/cell, V/cell, color = np.log(np.sqrt(U**2 + V**2)), density = 1, linewidth=1.5, cmap=plt.cm.OrRd)
+plt.xticks(range(120000/cell, 300000/cell, 20000/cell), x_label)
+plt.yticks(range(220000/cell, 280000/cell, 20000/cell), y_label)
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="2%", pad=0.3)
+plt.colorbar(strm.lines, cax=cax)
+#fig0.colorbar(strm.lines)
+ax.set_title('Flow Map of Puerto Rico (08/28/2017 7am to 10am)')
+plt.show()
 
